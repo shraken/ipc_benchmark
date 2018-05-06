8aab7e55c51f5483045b4c1281c37a81b6b5bee1
Nicholas Shrake <shraken@gmail.com>
2018-05-06 15:27:32 -0700

    add msg_queue example

--- msg_queue/client/client.c
+++ msg_queue/client/client.c
@@ -4,13 +4,17 @@
 #include <string.h>
 #include <fcntl.h>
 #include <time.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/msg.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
 
 #include <common.h>
 
-static char *fifo_name = "/tmp/ipc_fifo";
+static char *msgqueue_name = "../server/server.c";
 
 void print_runtime_stats(int total_bytes, int total_attempts, double total_time) {
     double average_throughput;
@@ -26,37 +30,21 @@ void print_runtime_stats(int total_bytes, int total_attempts, double total_time)
     return;
 }
 
-uint8_t *allocate_buffer(int total_size) {
-    int i;
-    int rand_value;
-    int total_bytes;
-    int total_attempts;
-    uint8_t *buffer;
-
-    srand(time(NULL));
-    buffer = (uint8_t *) malloc(total_size * 2);
-
-    for (i = 0; i < total_size; i++) {
-        rand_value = rand() % 0xFF;
-        *(buffer + i) = (uint8_t) rand_value;
-    }
-
-    return buffer;
-}
-
 void print_usage(char *argv[]) {
     printf("%s <block_size> <total_size>\n", argv[0]);
     return;
 }
 
 int main(int argc, char *argv[]) {
-    int fd;
     int n;
     int total_bytes;
     int total_attempts;
-    uint8_t *fbuf;
+    double total_time;
+    int msqid;
+    key_t key;
+    msg_queue_entry entry;
     struct timeval t_start, t_end;
-    
+
     if (argc < 3) {
         print_usage(argv);
         exit(0);
@@ -65,51 +53,48 @@ int main(int argc, char *argv[]) {
     int block_size = atoi(argv[1]);
     int total_size = atoi(argv[2]);
 
-    printf("named_pipe IPC server test\n");
+    printf("msg_queue IPC client test\n");
     printf("using block_size = %d\n", block_size);
     printf("using total_size = %d\n", total_size);
 
-    printf("allocating random buffer of length %d bytes\n", total_size);
-    fbuf = allocate_buffer(total_size);
-
-    if (!fbuf) {
-        printf("error could not allocate memory\n");
-        return -1;
-    } else {
-        printf("allocation finished\n");
+    //remove(msgqueue_name);
+    if ((key = ftok(msgqueue_name, 'B')) == -1) {
+        perror("ftok");
+        exit(1);
     }
 
-    fd = open(fifo_name, O_WRONLY);
-
-    if (fd == -1) {
-        perror("open");
-        return -1;
+    if ((msqid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+        perror("msgget");
+        exit(1);
     }
 
+    total_bytes = 0;
+    total_attempts = 0;
+    
+    gettimeofday(&t_start, NULL);
+    
     while (1) {
-        n = write(fd, fbuf + total_bytes, block_size);
+        if ((n = msgrcv(msqid, &entry, sizeof(entry.mtext), 0, 0)) == -1) {
+            perror("msgrcv");
+            exit(1);
+        }
 
-        if (n < 0) {
-            perror("write");
-            break;
-        } else if (n > 0) {
+        if (n > 0) {
             total_bytes += n;
             total_attempts++;
         }
 
-        if (total_bytes >= total_size * 2) {
+        if (total_bytes >= total_size) {
             break;
         }
     }
 
-    free(fbuf);
-    close(fd);
-
     gettimeofday(&t_end, NULL);
- 
+
     total_time = (double) (t_end.tv_sec - t_start.tv_sec);
     total_time += (double) (t_end.tv_usec - t_start.tv_usec) / 1000000;
 
     print_runtime_stats(total_bytes, total_attempts, total_time);
+
     return 0;
 }
--- msg_queue/server/server.c
+++ msg_queue/server/server.c
@@ -4,13 +4,17 @@
 #include <string.h>
 #include <fcntl.h>
 #include <time.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/msg.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
 
 #include <common.h>
 
-static char *fifo_name = "/tmp/ipc_fifo";
+static char *msgqueue_name = "server.c";
 
 void print_runtime_stats(int total_bytes, int total_attempts, double total_time) {
     double average_throughput;
@@ -50,13 +54,16 @@ void print_usage(char *argv[]) {
 }
 
 int main(int argc, char *argv[]) {
-    int fd;
     int n;
     int total_bytes;
     int total_attempts;
+    msg_queue_entry entry;
     uint8_t *fbuf;
+    key_t key;
+    int msqid;
+    double total_time;
     struct timeval t_start, t_end;
-    
+
     if (argc < 3) {
         print_usage(argv);
         exit(0);
@@ -65,7 +72,7 @@ int main(int argc, char *argv[]) {
     int block_size = atoi(argv[1]);
     int total_size = atoi(argv[2]);
 
-    printf("named_pipe IPC server test\n");
+    printf("msg_queue IPC server test\n");
     printf("using block_size = %d\n", block_size);
     printf("using total_size = %d\n", total_size);
 
@@ -79,31 +86,34 @@ int main(int argc, char *argv[]) {
         printf("allocation finished\n");
     }
 
-    fd = open(fifo_name, O_WRONLY);
-
-    if (fd == -1) {
-        perror("open");
-        return -1;
+    if ((key = ftok(msgqueue_name, 'B')) == -1) {
+        perror("ftok");
+        exit(1);
     }
+
+    if ((msqid = msgget(key, 0644)) == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    entry.mtype = 1;
 
     while (1) {
-        n = write(fd, fbuf + total_bytes, block_size);
+        memcpy(entry.mtext, fbuf + (total_bytes % total_size), sizeof(entry.mtext));
 
-        if (n < 0) {
-            perror("write");
-            break;
-        } else if (n > 0) {
-            total_bytes += n;
-            total_attempts++;
-        }
-
-        if (total_bytes >= total_size * 2) {
-            break;
+        if ((n = msgsnd(msqid, &entry, sizeof(entry.mtext), 0)) == -1) {
+            perror("msgsend");
         }
     }
 
+    printf("freeing memory buffer A\n");
     free(fbuf);
-    close(fd);
+    printf("freeing memory buffer B\n");
+
+    if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+        perror("msgctl");
+        exit(1);
+    }
 
     gettimeofday(&t_end, NULL);
  
@@ -111,5 +121,6 @@ int main(int argc, char *argv[]) {
     total_time += (double) (t_end.tv_usec - t_start.tv_usec) / 1000000;
 
     print_runtime_stats(total_bytes, total_attempts, total_time);
+    
     return 0;
 }
